4f3c0d0831f7aa48c994c683ad15f9bbe1fabd7e
jonataslaet <jonataslaetprogramador@gmail.com>
2023-05-10 14:42:14 -0300

    Created project

---
+++ backend-java/src/main/java/com/github/jonataslaet/laetsupermarket/LaetsupermarketApplication.java
@@ -0,0 +1,13 @@
+package com.github.jonataslaet.laetsupermarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LaetsupermarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LaetsupermarketApplication.class, args);
+	}
+
+}
---
+++ backend-java/src/test/java/com/github/jonataslaet/laetsupermarket/LaetsupermarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.jonataslaet.laetsupermarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LaetsupermarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
